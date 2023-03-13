b182d638fe457e9f7a7ef9a49241451baff61c10
philip-morlier <philipmorlier@gmail.com>
2023-03-13 12:12:51 -0700

    Added missing injection in core/state-transition.go
    All tests passing, all crucial plugins loading

--- core/state_transition.go
+++ core/state_transition.go
@@ -289,6 +289,13 @@ func (st *StateTransition) TransitionDb() (*ExecutionResult, error) {
 	// 6. caller has enough balance to cover asset transfer for **topmost** call
 
 	// Check clauses 1-3, buy gas if everything is correct
+
+	// begin PluGeth injection
+	if v, ok := st.evm.Config.Tracer.(PreTracer); ok {
+		v.CapturePreStart(st.msg.From(),  st.msg.To(), st.data, st.gas, st.msg.Value())
+		}
+	// end PluGeth injection 
+
 	if err := st.preCheck(); err != nil {
 		return nil, err
 	}
