392695aba6176c6a02cd84a659c844fa3f2b025c
philip-morlier <philipmorlier@gmail.com>
2023-05-10 17:48:20 -0700

    removed test files to enable circleCi build.

--- core/plugeth_dummy_test.go
+++ core/plugeth_dummy_test.go
@@ -5,6 +5,9 @@ import (
 )
 
 func TestAlwaysPasses(t *testing.T) {
+	// Perform any necessary setup or assertions here
+
+	// This assertion always passes
 	if true != true {
 		t.Errorf("Expected true, but got false")
 	}
