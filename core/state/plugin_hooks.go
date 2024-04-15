1ea7d277ebd1fef94cab82378a04477ce8b0bde8
Austin Roberts <austin.roberts@rivet.cloud>
2024-04-15 10:00:49 -0500

    Prune destructs on empty accounts from stateUpdate hook

--- core/state/plugin_hooks.go
+++ core/state/plugin_hooks.go
@@ -45,18 +45,27 @@ func PluginStateUpdate(pl *plugins.PluginLoader, blockRoot, parentRoot common.Ha
 	})
 	coreDestructs := make(map[core.Hash]struct{})
 	for k, v := range destructs {
-		if _, ok := accounts[k]; ok && !checker.hadStorage(k) {
-			// If an account is in both destructs and accounts, that means it was
-			// "destroyed" and recreated in the same block. Especially post-cancun,
-			// that generally means that an account that had ETH but no code got
-			// replaced by an account that had code.
-			//
-			// If there's data in the accounts map, we only need to process this
-			// account if there are storage slots we need to clear out, so we
-			// check the account storage for the empty root. If it's empty, we can
-			// skip this destruct. We need this check to normalize parallel blocks
-			// with serial blocks, because they report destructs differently.
-			continue
+		if _, ok := accounts[k]; ok {
+			if !checker.hadStorage(k) {
+				// If an account is in both destructs and accounts, that means it was
+				// "destroyed" and recreated in the same block. Especially post-cancun,
+				// that generally means that an account that had ETH but no code got
+				// replaced by an account that had code.
+				//
+				// If there's data in the accounts map, we only need to process this
+				// account if there are storage slots we need to clear out, so we
+				// check the account storage for the empty root. If it's empty, we can
+				// skip this destruct. We need this check to normalize parallel blocks
+				// with serial blocks, because they report destructs differently.
+				continue
+			}
+		} else {
+			if !checker.exists(k) {
+				// If we have a destruct for an account that didn't exist at the previous
+				// block, we don't need to destruct it. This normalizes the use case of
+				// CREATE and CREATE2 deployments to empty addresses that revert.
+				continue
+			}
 		}
 		coreDestructs[core.Hash(k)] = v
 	}
@@ -101,6 +110,31 @@ type acctChecker struct {
 	trie Trie
 }
 
+func (ac *acctChecker) exists(k common.Hash) bool {
+	if hadStorage, ok := ac.snapExists(k); ok {
+		return hadStorage
+	}
+	return ac.trieExists(k)
+}
+
+func (ac *acctChecker) snapExists(k common.Hash) (bool, bool) {
+	acct, err := ac.snap.AccountRLP(k)
+	if err != nil {
+		return false, false
+	}
+	return len(acct) != 0, true
+}
+
+func (ac *acctChecker) trieExists(k common.Hash) bool {
+	trie, ok := ac.trie.(acctByHasher)
+	if !ok {
+		log.Warn("Couldn't check trie existence, wrong trie type")
+		return true
+	}
+	acct, _ := trie.GetAccountByHash(k)
+	return acct != nil
+}
+
 func (ac *acctChecker) hadStorage(k common.Hash) bool {
 	if hadStorage, ok := ac.snapHadStorage(k); ok {
 		return hadStorage
@@ -110,8 +144,12 @@ func (ac *acctChecker) hadStorage(k common.Hash) bool {
 
 func (ac *acctChecker) snapHadStorage(k common.Hash) (bool, bool) {
 	acct, err := ac.snap.Account(k)
-	if err != nil || acct == nil {
+	if err != nil {
 		return false, false
+	}
+	if acct == nil {
+		// No account existed at this hash, so it wouldn't have had storage
+		return false, true
 	}
 	if len(acct.Root) > 0 && !bytes.Equal(acct.Root, types.EmptyRootHash.Bytes()) {
 		return true, true
@@ -128,6 +166,10 @@ func (ac *acctChecker) trieHadStorage(k common.Hash) bool {
 	acct, err := trie.GetAccountByHash(k)
 	if err != nil {
 		return true
+	}
+	if acct == nil {
+		// No account existed at this hash, so it wouldn't have had storage
+		return false
 	}
 	return acct.Root != types.EmptyRootHash
 }
